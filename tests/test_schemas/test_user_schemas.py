a0e784c1a70ea614f549183ef28fe445a67d661e
vinaykath <vvk28@njit.edu>
2024-07-25 22:43:10 -0400

    Officially removed comments from branch

--- tests/test_schemas/test_user_schemas.py
+++ tests/test_schemas/test_user_schemas.py
@@ -7,20 +7,20 @@ from app.schemas.user_schemas import UserBase, UserCreate, UserUpdate, UserRespo
 # Tests for UserBase
 def test_user_base_valid(user_base_data):
     user = UserBase(**user_base_data)
-    # REMOVED assert user.nickname == user_base_data["nickname"]
+    #  assert user.nickname == user_base_data["nickname"]
     assert user.email == user_base_data["email"]
 
 # Tests for UserCreate
 def test_user_create_valid(user_create_data):
     user = UserCreate(**user_create_data)
-    # REMOVED assert user.nickname == user_create_data["nickname"]
+    #  assert user.nickname == user_create_data["nickname"]
     assert user.password == user_create_data["password"]
 
 # Tests for UserUpdate
 def test_user_update_valid(user_update_data):
     user_update = UserUpdate(**user_update_data)
     assert user_update.email == user_update_data["email"]
-    # REMOVED assert user_update.first_name == user_update_data["first_name"]
+    #  assert user_update.first_name == user_update_data["first_name"]
 
 # Tests for UserResponse
 def test_user_response_valid(user_response_data):
